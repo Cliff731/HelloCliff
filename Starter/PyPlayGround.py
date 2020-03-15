b80f7bbc2e4efb2e6610e31a37e35fbd230f37a7
Cliff731 <clifflefevers@gmail.com>
2020-03-14 20:34:34 -0400

    commit

--- Starter/PyPlayGround.py
+++ Starter/PyPlayGround.py
@@ -1,3 +1,6 @@
+#imports
+import math
+
 #var
 savings = 100
 savings += 50
@@ -7,7 +10,7 @@ for i in range(5):
     x = i * 10
     print("x =", x)
 
-import  math
+
 s = math.sqrt(81)
 print('square root of 81 = ', s)
 print(type(s))
@@ -15,7 +18,9 @@ print(type(s))
 f = math.factorial(5)
 print ('factorial of 5 = ', f)
 print (type(f))
+
+
 print ('bye')
 
-print(savings)
+
 
